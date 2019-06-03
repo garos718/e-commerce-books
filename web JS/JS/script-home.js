4961a584b1b6d8c434e5aa830db7841ac93612ac
Esteban Garós <esteban.garos.blasco@gmail.com>
2019-06-03 12:55:37 +0200

    first release

--- web JS/JS/script-home.js
+++ web JS/JS/script-home.js
@@ -2,11 +2,9 @@ function validate() {
     var search;
     search = document.getElementById("search");
     
-    if(search.length>3) {
-        document.getElementById("search").classList.remove("is-invalid");
-        document.getElementById("search").classList.add("is-valid");
+    if(search.length<=3) {
+        document.getElementById("search-btn").disabled = true;
     } else {
-        document.getElementById("search").classList.remove("is-valid");
-        document.getElementById("search").classList.add("is-invalid");
+        document.getElementById("search-btn").disabled = false;
     }
 }
